2668f484c0bbff13f9df977dc338d52ac1ef04ea
Almaz59 <100568607+Almaz59@users.noreply.github.com>
2022-07-15 17:33:10 +0500

    course3 hw 2 first commit

---
+++ src/main/java/ru/hogwarts/school/Course3Hw2SchoolApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3Hw2SchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course3Hw2SchoolApplication.class, args);
+    }
+
+}
